15fdf70722224a4845dadfce27f64d62374327f3
MauriceBruijn <bruijn80@gmail.com>
2018-03-26 13:30:14 +0200

    style

--- pao_project_MML/resources/views/products/edit.blade.php
+++ pao_project_MML/resources/views/products/edit.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading"><center>Product toevoegen</center></div>
+                    <div class="panel-heading"><center>Product aanpassen</center></div>
 
                     <div class="panel-body">
 
--- pao_project_MML/resources/views/products/index.blade.php
+++ pao_project_MML/resources/views/products/index.blade.php
@@ -11,47 +11,59 @@
 						@foreach($products as $product)
 							<div class="product">
 								<div class="products_title">
-									<div><a href="product/get/{{$product->id}}">{{$product->name}}</a></div>
-								</div>
-
-								<div class="product_image">
-									<img src="https://sloanreview.mit.edu/content/uploads/2017/08/MAG-FR-Oestreicher-Singer-Product-Recommendation-Viral-Marketing-Social-Media-Network-Ecommerce-1200-1200x627.jpg" alt="Mountain View" width="200" height="200">
-								</div>
-
-								<div class="product_info">
-									<div class="product_code">Product nr. {{$product->id}}</div>
-									<hr/>
-									<div class="product_details">{{$product->brand}}</div>
-								</div>
-
-								<div class="product_price">
-									€ {{$product->price}}
-								</div>
-							</div>
-							
-							<div>
-								@if (Auth::user()->rank >= 1)
-									<span class="col-md-0 col-md-offset-10">
-										<a href="product/edit/{{$product->id}}" style="top:30px;" class="glyphicon glyphicon-pencil"></a>
-									</span>
-								@endif
-								@if (Auth::user()->rank >= 2)
-									<form action="delete/product/{{$product->id}}" method="post">
-										{{method_field('DELETE')}}
-										{{csrf_field()}}
+									
+									<div class="product_image">
+										<img src="{{$product->img}}" alt="Mountain View" width="200" height="200"/>
+									</div>
+									
+									<table class="table table-reflow table_product_details">
+										<tr>
+											<th>ID</th>
+											<th>NAAM</th>
+											<th>MERK</th>
+											<th>€</th>
+										</tr>
 										
-										<div class="col-md-0 col-md-offset-11">
-											<button type="submit" class="btn btn-danger">
-												X
-											</button>
+										<tr>
+											<td>{{$product->id}}</td>
+											<td>{{$product->name}}</td>
+											<td>{{$product->brand}}</td>
+											<td>{{$product->price}}</td>
+										</tr>
+									</table>
+									
+									<div class="form-group">
+										<div class="col-md-8 col-md-offset-10">
+											<a href="product/get/{{$product->id}}">
+												<button type="submit" class="btn btn-primary">
+													BEKIJKEN
+												</button>
+											</a>
 										</div>
-									</form>
-								@endif
-							</div>
-							
-							<hr/>
-						@endforeach
-					</div>
+									</div>
+								
+									<div>
+										@if (Auth::user()->rank >= 1)
+											<span class="col-md-0 col-md-offset-10">
+												<a href="product/edit/{{$product->id}}" style="top:30px;" class="glyphicon glyphicon-pencil"></a>
+											</span>
+										@endif
+										@if (Auth::user()->rank >= 2)
+											<form action="delete/product/{{$product->id}}" method="post">
+												{{method_field('DELETE')}}
+												{{csrf_field()}}
+												
+												<div class="col-md-0 col-md-offset-11">
+													<button type="submit" class="btn btn-danger">
+														X
+													</button>
+												</div>
+											</form>
+										@endif
+									</div>
+								</div>
+							@endforeach
+						</div>
 					
 
 					@if (Auth::user()->rank >= 1)
@@ -65,6 +77,7 @@
 							</div>
 						</div>
 					@endif
+					</div>
 				</div>
 			</div>
 		</div>
