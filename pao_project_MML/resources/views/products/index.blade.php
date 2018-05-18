c141f662876e502cd68dae3ae668fcb1c6d2d48a
MauriceBruijn <bruijn80@gmail.com>
2018-05-18 09:21:34 +0200

    style

--- pao_project_MML/resources/views/products/index.blade.php
+++ pao_project_MML/resources/views/products/index.blade.php
@@ -3,72 +3,62 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-body">
+		@foreach($products as $product)
+		
+		<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+			<div class="db-wrapper">
+				<div class="db-pricing-eleven db-bk-color-one">
 				
-					<div class="box_products">
-						<div class="products_title"><i>PRODUCTEN</i></div>
+					<div class="price">
+						<sup>€</sup>{{$product->price}}
+					</div>
+					
+					<div class="type">
+						{{$product->name}}
+					</div>
+					
+					<ul>
+						<li><i class="glyphicon glyphicon-print"></i>{{$product->brand}}</li>
+						<li><i class="glyphicon glyphicon-time"></i>{{$product->color}}</li>
 						
-						<table class="table table-reflow">
-						<tr>
-							<th>ID</th>
-							<th>NAAM</th>
-							<th>MERK</th>
-							<th>KLEUR</th>
-							<th>PRIJS</th>
-							
-							@if (Auth::user()->rank >= 1)
-								<th></th>
-								<th></th>
-							@endif
-						</tr>
-						@foreach($products as $product)
-						<tr>
-							<td>{{$product->id}}</td>
-							<td><a href="product/get/{{$product->id}}">{{$product->name}}</a></td>
-							<td>{{$product->brand}}</td>
-							<td>{{$product->color}}</td>
-							<td>{{$product->price}}</td>
-							
-							@if (Auth::user()->rank >= 1)
-							<td>
-								<a href="product/edit/{{$product->id}}" class="glyphicon glyphicon-pencil"></a>
-							</td>
+						@if (Auth::user()->rank >= 1)
+							<li><a href="product/edit/{{$product->id}}" class="glyphicon glyphicon-pencil"></a></li>
 						@endif
-							
+						
 						@if (Auth::user()->rank >= 2)
-							<td>
+							<li>
 								<form action="delete/product/{{$product->id}}" method="post">
-								{{method_field('DELETE')}}
-								{{csrf_field()}}
+									{{method_field('DELETE')}}
+									{{csrf_field()}}
 								
-								<button type="submit" class="btn btn-danger">
+									<button type="submit" class="btn btn-danger">
 										X
 									</button>
 								</form>
-							</td>
+							</li>
 						@endif
-							
-						</tr>
-						@endforeach
-						</table>
-						
-						@if (Auth::user()->rank >= 1)
-							<div class="form-group">
-								<div class="col-md-8 col-md-offset-0">
-									<a href="product/new">
-										<button type="submit" class="btn btn-primary">
-											Toevoegen
-										</button>
-									</a>
-								</div>
-							</div>
-						@endif
+					</ul>
+					
+					<div class="pricing-footer">
+						<a href="product/get/{{$product->id}}" class="btn btn-primary">VIEW</a>
 					</div>
 				</div>
 			</div>
 		</div>
+		@endforeach
+		
 	</div>
+	
+	@if (Auth::user()->rank >= 1)
+		<div class="form-group">
+			<div class="col-md-8 col-md-offset-0">
+				<a href="product/new">
+					<button type="submit" class="btn btn-primary">
+						Toevoegen
+					</button>
+				</a>
+			</div>
+		</div>
+	@endif
 </div>
 @endsection
